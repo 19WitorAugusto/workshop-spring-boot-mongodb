e9c0e7df3b2d6ceb5cb02d2bb5b452e1c0069007
witor augusto <witorsantosaugusto@gmail.com>
2020-07-11 12:14:21 -0300

    Referenciando os posts do usuário

--- src/main/java/com/witoraugusto/worckshopMongo/configuration/Instantiation.java
+++ src/main/java/com/witoraugusto/worckshopMongo/configuration/Instantiation.java
@@ -42,6 +42,8 @@ public class Instantiation implements CommandLineRunner {
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
+		u1.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(u1);
 	}
 
 }
